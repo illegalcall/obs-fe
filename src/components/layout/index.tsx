b52469a4e80b0cfec9e856953fe67a44aaaf4b88
Dhruv Sharma <dhruvsharma@cult>
2023-08-29 01:02:55 +0530

    login integration test implmented

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,7 +6,17 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <div className="h-[100vh]">
       <Header />
       <div className="flex items-center justify-center px-8 pt-[100px] h-full">{children}</div>
-    </div>
+      <footer className="absolute text-xs bottom-4 w-full text-center">
+        WFH 2023 --{" "}
+        <span
+          data-cy="layout-footer-title"
+          className="italic text-black-400">
+          Designed with 💜 by{" "}
+          Dhruv, Shriharsha, Biparnak, Pankaj
+
+        </span>
+      </footer>
+    </div >
   )
 }
 
